bbe91e1f4a3a5fe4ee78e4527ac51ae21550c969
Panchoa <francois.montagne2@free.fr>
2019-04-16 11:48:13 +0200

    Création du backend servant à push et get des données dans Redis

---
+++ www/backend.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Panchoa
+ * Date: 16/04/2019
+ * Time: 10:53
+ */
+require "predis/autoload.php";
+\Predis\Autoloader::register();
+
+$redisObj = new Redis();
+
+function openRedisConnection($hostname, $port){
+    global $redisObj;
+    $redisObj->connect($hostname, $port);
+    return $redisObj;
+}
+
+//Ajout de valeur donnée pour la clé entrée en paramètre dans Redis
+function setNote($key, $value){
+    try{
+        global $redisObj;
+        $redisObj->set($key, $value);
+    }catch (Exception $e) {
+        echo $e->getMessage();
+    }
+}
+
+//Récupère les valeurs en fonction de la clé donnée en paramètre
+function getNote($key){
+    try{
+        global $redisObj;
+        return $redisObj->get($key);
+    }catch (Exception $e){
+        echo $e->getMessage();
+    }
+}
